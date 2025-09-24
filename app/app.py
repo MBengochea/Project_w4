19a8187d789f8b20f8c4e24ea77a026da46cac80
Mauricio Bengochea Torres <Masteramida@gmail.com>
2025-09-24 19:37:32 +0200

    Fix error with app.py code after ignacio's help, was HEAD >>>> in the code

--- app/app.py
+++ app/app.py
@@ -39,11 +39,8 @@ st.write("Find the best board games for your session based on your preferences."
 # --- User Inputs ---
 playtime = st.slider("⏱️ Desired playtime (minutes)", 0, 1200, 30)
 number_players = st.slider("👥 Number of players", 1, 100, 2)
-<<<<<<< HEAD
 min_age = st.slider("🧒 Age of youngest player", 0, 18)
-=======
 min_age = st.slider("🧒 Age of youngest player", 4, 18, 12)
->>>>>>> ebfb4270c10172201f2fd1cd15f2bf350db49104
 difficulty_level = st.selectbox("🧠 Desired difficulty level", [1, 2, 3, 4], format_func=lambda x: ["Easy", "Medium", "Hard", "Very Hard"][x-1])
 complexity = float(difficulty_level)
 
